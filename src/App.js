4d5b249ae7fe03cb07c466a3536629d7ef8f9a15
SujayGG <sujaygonchigar@gmail.com>
2025-06-29 20:43:26 -0500

    UI and stability updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { AppBar, Toolbar, Button, Container, Typography } from '@mui/material';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
 import './App.css';
 
 function App() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Welcome to Care4Youth Website</h1>
-      <p>This is your new React website. Start building your UI here.</p>
-      <button onClick={() => alert('Button clicked!')}>Click Me</button>
-    </div>
+    <Router>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Care4Youth
+          </Typography>
+          <Button color="inherit" component={Link} to="/">Home</Button>
+          <Button color="inherit" component={Link} to="/about">About</Button>
+          <Button color="inherit" component={Link} to="/contact">Contact</Button>
+        </Toolbar>
+      </AppBar>
+      <Container sx={{ mt: 4 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
